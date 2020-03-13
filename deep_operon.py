1271aca3d465e4b1768397388e8620c61a9ee1c4
Rinoahu <xiaohu@iastate.edu>
2020-03-13 13:39:48 -0600

    change the project name

--- deep_operon.py
+++ deep_operon.py
@@ -71,142 +71,111 @@ except:
 
 
 
+from tensorflow.python.keras import backend as K_tf
+from tensorflow.python.keras.optimizers import Optimizer_tf
 
-from tensorflow.keras import backend as K
-#from tensorflow.keras.optimizers import Optimizer
-from keras.optimizers import Optimizer
-
-class AdaBound(Optimizer):
+class AdaBound(Optimizer_tf):
     """AdaBound optimizer.
-
     Default parameters follow those provided in the original paper.
-
-    # Arguments
+    Arguments:
         lr: float >= 0. Learning rate.
-        final_lr: float >= 0. Final learning rate.
         beta_1: float, 0 < beta < 1. Generally close to 1.
         beta_2: float, 0 < beta < 1. Generally close to 1.
-        gamma: float >= 0. Convergence speed of the bound function.
+        final_lr: float >= 0. Final learning rate.
+        gamma: float >= 0. Convergence speed of the bound functions.
         epsilon: float >= 0. Fuzz factor. If `None`, defaults to `K.epsilon()`.
         decay: float >= 0. Learning rate decay over each update.
-        weight_decay: Weight decay weight.
-        amsbound: boolean. Whether to apply the AMSBound variant of this
-            algorithm.
-
-    # References
-        - [Adaptive Gradient Methods with Dynamic Bound of Learning Rate]
-          (https://openreview.net/forum?id=Bkg3g2R9FX)
-        - [Adam - A Method for Stochastic Optimization]
-          (https://arxiv.org/abs/1412.6980v8)
-        - [On the Convergence of Adam and Beyond]
-          (https://openreview.net/forum?id=ryQu7f-RZ)
+        amsbound: boolean. Whether to use the AMSBound variant of this algorithm
+            from the paper "Adaptive Gradient Methods with Dynamic Bound of Learning Rate".
     """
 
-    def __init__(self, lr=0.001, final_lr=0.1, beta_1=0.9, beta_2=0.999, gamma=1e-3,
-                 epsilon=None, decay=0., amsbound=False, weight_decay=0.0, **kwargs):
+    def __init__(self,
+                 lr=0.001,
+                 beta_1=0.9,
+                 beta_2=0.999,
+                 final_lr=0.1,
+                 gamma=0.001,
+                 epsilon=1e-8,
+                 decay=0.,
+                 amsbound=False,
+                 **kwargs):
         super(AdaBound, self).__init__(**kwargs)
-
-        if not 0. <= gamma <= 1.:
-            raise ValueError("Invalid `gamma` parameter. Must lie in [0, 1] range.")
-
-        with K.name_scope(self.__class__.__name__):
-            self.iterations = K.variable(0, dtype='int64', name='iterations')
-            try:
-                self.lr = K.variable(lr, name='lr')
-            except:
-                self.learning_rate = K.variable(lr, name='lr')
-            self.beta_1 = K.variable(beta_1, name='beta_1')
-            self.beta_2 = K.variable(beta_2, name='beta_2')
-            self.decay = K.variable(decay, name='decay')
-
-        self.final_lr = final_lr
-        self.gamma = gamma
-
+        with K_tf.name_scope(self.__class__.__name__):
+            self.iterations = K_tf.variable(0, dtype='int64', name='iterations')
+            self.lr = K_tf.variable(lr, name='lr')
+            self.beta_1 = K_tf.variable(beta_1, name='beta_1')
+            self.beta_2 = K_tf.variable(beta_2, name='beta_2')
+            self.final_lr = K_tf.variable(final_lr, name='final_lr')
+            self.gamma = K_tf.variable(gamma, name='gamma')
+            self.decay = K_tf.variable(decay, name='decay')
+            self.amsbound = K_tf.variable(amsbound, name='amsbound')
         if epsilon is None:
-            epsilon = K.epsilon()
-        self.epsilon = epsilon
+            epsilon = K_tf.epsilon()
+        self.epsilon = K_tf.variable(epsilon)
         self.initial_decay = decay
-        self.amsbound = amsbound
-
-        self.weight_decay = float(weight_decay)
-        self.base_lr = float(lr)
+        self.base_lr = lr
 
     def get_updates(self, loss, params):
         grads = self.get_gradients(loss, params)
-        self.updates = [K.update_add(self.iterations, 1)]
+        self.updates = [state_ops.assign_add(self.iterations, 1)]
 
         lr = self.lr
         if self.initial_decay > 0:
-            lr = lr * (1. / (1. + self.decay * K.cast(self.iterations,
-                                                      K.dtype(self.decay))))
+            lr = lr * (
+                    1. / (1. + self.decay * math_ops.cast(self.iterations,
+                                                          K_tf.dtype(self.decay))))
 
-        t = K.cast(self.iterations, K.floatx()) + 1
-
-        # Applies bounds on actual learning rate
-        step_size = lr * (K.sqrt(1. - K.pow(self.beta_2, t)) /
-                          (1. - K.pow(self.beta_1, t)))
+        t = math_ops.cast(self.iterations, K_tf.floatx()) + 1
+        lr_t = lr * (
+                K_tf.sqrt(1. - math_ops.pow(self.beta_2, t)) /
+                (1. - math_ops.pow(self.beta_1, t)))
 
         final_lr = self.final_lr * lr / self.base_lr
-        lower_bound = final_lr * (1. - 1. / (self.gamma * t + 1.))
+        lower_bound = final_lr * (1. - 1. / (self.gamma * t + 1))
         upper_bound = final_lr * (1. + 1. / (self.gamma * t))
 
-        ms = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
-        vs = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+        ms = [K_tf.zeros(K_tf.int_shape(p), dtype=K_tf.dtype(p)) for p in params]
+        vs = [K_tf.zeros(K_tf.int_shape(p), dtype=K_tf.dtype(p)) for p in params]
         if self.amsbound:
-            vhats = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+            vhats = [K_tf.zeros(K_tf.int_shape(p), dtype=K_tf.dtype(p)) for p in params]
         else:
-            vhats = [K.zeros(1) for _ in params]
+            vhats = [K_tf.zeros(1) for _ in params]
         self.weights = [self.iterations] + ms + vs + vhats
 
         for p, g, m, v, vhat in zip(params, grads, ms, vs, vhats):
-            # apply weight decay
-            if self.weight_decay != 0.:
-                g += self.weight_decay * K.stop_gradient(p)
-
             m_t = (self.beta_1 * m) + (1. - self.beta_1) * g
-            v_t = (self.beta_2 * v) + (1. - self.beta_2) * K.square(g)
-
+            v_t = (self.beta_2 * v) + (1. - self.beta_2) * math_ops.square(g)
             if self.amsbound:
-                vhat_t = K.maximum(vhat, v_t)
-                denom = (K.sqrt(vhat_t) + self.epsilon)
-                self.updates.append(K.update(vhat, vhat_t))
+                vhat_t = math_ops.maximum(vhat, v_t)
+                p_t = p - m_t * K_tf.clip(lr_t / (K_tf.sqrt(vhat_t) + self.epsilon), lower_bound, upper_bound)
+                self.updates.append(state_ops.assign(vhat, vhat_t))
             else:
-                denom = (K.sqrt(v_t) + self.epsilon)
+                p_t = p - m_t * K_tf.clip(lr_t / (K_tf.sqrt(v_t) + self.epsilon), lower_bound, upper_bound)
 
-            # Compute the bounds
-            step_size_p = step_size * K.ones_like(denom)
-            step_size_p_bound = step_size_p / denom
-            bounded_lr_t = m_t * K.minimum(K.maximum(step_size_p_bound,
-                                                     lower_bound), upper_bound)
-
-            p_t = p - bounded_lr_t
-
-            self.updates.append(K.update(m, m_t))
-            self.updates.append(K.update(v, v_t))
+            self.updates.append(state_ops.assign(m, m_t))
+            self.updates.append(state_ops.assign(v, v_t))
             new_p = p_t
 
             # Apply constraints.
             if getattr(p, 'constraint', None) is not None:
                 new_p = p.constraint(new_p)
 
-            self.updates.append(K.update(p, new_p))
+            self.updates.append(state_ops.assign(p, new_p))
         return self.updates
 
     def get_config(self):
-        config = {'lr': float(K.get_value(self.lr)),
-                  'final_lr': float(self.final_lr),
-                  'beta_1': float(K.get_value(self.beta_1)),
-                  'beta_2': float(K.get_value(self.beta_2)),
-                  'gamma': float(self.gamma),
-                  'decay': float(K.get_value(self.decay)),
-                  'epsilon': self.epsilon,
-                  'weight_decay': self.weight_decay,
-                  'amsbound': self.amsbound}
+        config = {
+            'lr': float(K_tf.get_value(self.lr)),
+            'beta1': float(K_tf.get_value(self.beta_1)),
+            'beta2': float(K_tf.get_value(self.beta_2)),
+            'final_lr': float(K_tf.get_value(self.final_lr)),
+            'gamma': float(K_tf.get_value(self.gamma)),
+            'decay': float(K_tf.get_value(self.decay)),
+            'epsilon': self.epsilon,
+            'amsbound': self.amsbound
+        }
         base_config = super(AdaBound, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
-
-
-
 
 
 from pickle import load, dump
